d223b7282975defa7cb7baa042846265d02d38d3
Amir Mann <Amir-Mann@github.com>
2024-07-29 17:37:26 +0300

    Untested code for split edge and edge flip

--- remeshing/remesh.py
+++ remeshing/remesh.py
@@ -2,32 +2,65 @@
 
 import json
 
+
+class Mesh():
+    def __init__(self, verts=[], faces=[]):
+        self.verts: list[Vertex] = verts
+        self.faces: list[Face] = faces
+
+
 class Vertex():
-    def __init__(self, x, y, z):
+    def __init__(self, x:float, y:float, z:float):
         self.x = x
         self.y = y
         self.z = z
-        self.faces = []
+        self.faces: list[Face] = []
         self.boundary = False
     
-    def unite(self, other):
+    def unite(self, other:'Vertex', mesh:Mesh):
         self.x = (self.x + other.x) / 2
         self.y = (self.y + other.y) / 2
         self.z = (self.z + other.z) / 2
         for face in other.faces[:]:
             if self in face.verts:
                 face.delete()
+                mesh.faces.remove(face)
                 continue
             face.verts[face.verts.index(other)] = self
             self.faces.append(face)
         other.faces = []
+        mesh.verts.remove(other)
+        
     
+    def __add__(self, other):
+        return Vertex(self.x + other.x, self.y + other.y, self.z + other.z)
+    
+    def __idiv__(self, other:float):
+        self.x /= other
+        self.y /= other
+        self.z /= other
+
+    def split_edge(self, other:'Vertex', mesh:Mesh):
+        mid_point = self + other
+        mid_point /= 2
+        for face in self.faces:
+            if face in other.faces:
+                # Move the face to the mid point
+                face_copy = Face.Copy(face)
+                face.switch(self, mid_point)
+                face_copy.switch(other, mid_point)
+                mesh.faces.append(face_copy)
+        mesh.verts.append(mid_point)
+        
+
+
+
     def __repr__(self):
         return f"V({self.x:.2f}, {self.y:.2f}, {self.z:.2f})"
 
 class Face():
     def __init__(self, verts):
-        self.verts = verts
+        self.verts: list[Vertex] = verts
         for vert in verts:
             vert.faces.append(self)
     
@@ -37,6 +70,16 @@ class Face():
     
     def __repr__(self):
         return f"F({self.verts})"
+
+    @staticmethod
+    def Copy(other:'Face'):
+        return Face(other.verts[:])
+
+    def switch(self, old_vertex, new_vertex):
+        index = self.verts.index(old_vertex)
+        self.verts[index].faces.remove(self)
+        self.verts[index] = new_vertex
+        self.verts[index].faces.append(self)
 
 
 data = json.loads(open("./samples/sample1.json", "r").read())
